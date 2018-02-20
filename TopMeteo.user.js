49fc6436d66113e5f26cc57a6f8a6ecc052aae72
DaanA32 <daan.adams@student.ucll.be>
2018-02-20 20:17:06 +0100

    fix for different languages / countries

--- TopMeteo.user.js
+++ TopMeteo.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  UserScript of JS written by Nathagamelle http://www.parapentebelge.be/parapentebelge/forum/list.php?id=Script%20TopMeteo
 // @author       Daan
-// @include      /^https?:\/\/europe.topmeteo.eu\/en\/fr\/loc\/[0-9]+/.*
+// @include      /^https?:\/\/europe.topmeteo.eu\/[A-z]{2,5}\/[A-z]{2,5}\/loc\/[0-9]+/.*
 // @grant        none
 // ==/UserScript==
 $("body").html(String($('body').html()).replace(/([0-9]*[05])\°\/([0-9]+)/g, '<svg height="20" width="20" version="1.1"><g transform="scale(0.2) rotate($1,50,50) rotate(180,50,50) translate(0,5)"><path stroke-width="0" fill="black" d="m50,0 -20,30 16,-3 -3,63 14,0 -3,-63 16,3 -20,-30z"></path></g></svg><vel style="font-size:15px">$2</vel>'));
